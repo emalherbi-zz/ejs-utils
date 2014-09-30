3a16a7e64f498fad3d9498fb06f0b1f0f657914c
Eduardo Malherbi Martins <emalherbi@gmail.com>
2014-09-30 13:53:31 -0300

    update project

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+  'use strict';
+
+	// Load the plugin that clean files and directories.
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	// Load the plugin that copy files and directories.
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	// Load the plugin that minify and concatenate ".js" files.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Automatic notifications when tasks fail.
+	grunt.loadNpmTasks('grunt-notify');
+
+  grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+
+    /* clean directories */
+    clean: ['dist'],
+
+		/* put files not handled in other tasks here */
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+            dot: true,
+            dest: 'dist',
+            src: ['ejs-utils.js']
+          }]
+        }
+    },
+
+    /* js file minification */
+    uglify: {
+      options: {
+        preserveComments: false
+      },
+			build: {
+				src: 'dist/<%= pkg.name %>.js',
+				dest: 'dist/<%= pkg.name %>.min.js'
+			}
+    },
+
+	});
+
+	// tasks
+  grunt.registerTask('build', [
+      'clean',
+      'copy',
+      'uglify',
+  ]);
+
+  grunt.registerTask('deploy', [
+      'build'
+  ]);
+
+  grunt.registerTask('default', [
+      'build'
+  ]);
+};
