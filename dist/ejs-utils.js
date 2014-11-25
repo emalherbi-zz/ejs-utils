9203eb1c55b2e9a24ed8f45a4b0366e33332a738
Eduardo Malherbi <emalherbi@gmail.com>
2014-11-25 01:04:44 -0200

    auto-generated commit

--- dist/ejs-utils.js
+++ dist/ejs-utils.js
@@ -1,5 +1,5 @@
 /*!
- * ejs-utils v2.0.20 (http://emalherbi.github.io/ejs-utils/)
+ * ejs-utils v2.0.21 (http://emalherbi.github.io/ejs-utils/)
  * Copyright 2010-2014 emalherbi
  * Licensed under MIT (http://en.wikipedia.org/wiki/MIT_License)
  */
